dfdb129cd1655d8b9eba7999d53f4fab5c971f50
Juniper Song <songminseonk@gmail.com>
2022-09-28 12:20:56 -0400

    git ignore file and hello  world project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// This is our main entry point of the application
+//The computer go here first to look for instructions to execute
+
+Console.WriteLine("Hello, World!");
