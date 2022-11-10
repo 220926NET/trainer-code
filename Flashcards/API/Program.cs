09ddd976fd7a43ed8b4f9af6e7ed452492501a5e
Juniper Song <songminseonk@gmail.com>
2022-11-10 11:32:19 -0500

    ef core

--- Flashcards/API/Program.cs
+++ Flashcards/API/Program.cs
@@ -1,12 +1,15 @@
 using DataAccess;
 using Services;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<SqlConnectionFactory>(ctx => new SqlConnectionFactory(builder.Configuration.GetConnectionString("flashcardDB")));
-builder.Services.AddScoped<IFlashCardStorage, FlashCardRepo>();
+builder.Services.AddDbContext<FlashcardsDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("flashcardDB")));
+builder.Services.AddScoped<IFlashCardStorage, FlashcardEFDbFirstRepo>();
 builder.Services.AddScoped<FlashCardService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
